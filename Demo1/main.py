93b06a182018d3f6684fdb7009cde96d69fc817a
kurokawa77 <kurokawa77@gmail.com>
2024-02-11 11:22:19 +0800

    This is the first change2

--- Demo1/main.py
+++ Demo1/main.py
@@ -16,7 +16,31 @@ duo_integration_key = env.DUO_INTEGRATION_KEY
 duo_secret_key = env.DUO_SECRET_KEY
 duo_api_host = env.DUO_API_HOST
 
-print(duo_integration_key)
+admin_api = duo_client.Admin(
+    ikey=duo_integration_key,
+    skey=duo_secret_key,
+    host=duo_api_host
+)
+
+users = admin_api.get_users()
+
+#Disable or Activate the user
+#user_id = ""
+#for user in users:
+#    if user['username'] == "granite":
+#        user_id = user['user_id']
+#        print(f"\nThis is the user ID of {user['username']}: {user_id}")
+
+# disable a user
+#admin_api.update_user(user_id=user_id, status="disabled")
+#admin_api.update_user(user_id=user_id, status="active")
+
+#users = admin_api.get_users()
+
+print("Username\t\tStatus")
+print("------------------")
+for user in users:
+    print(f"{user['username']}\t{user['status']}")
 #
 #
 #
